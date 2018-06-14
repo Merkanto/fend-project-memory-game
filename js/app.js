e9a52ddfee39448a3fd7973a3f6c73acdb372107
Miroslav-Zhelezchev <miroslav.i.zhelezchev@gmail.com>
2018-06-14 20:33:39 +0300

    add all javascript changes

--- js/app.js
+++ js/app.js
@@ -1,33 +1,45 @@
 /*
  * Create a list that holds all of your cards
  */
-const cardsHolderArr = ['paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'bomb', 'bomb', 'diamond', 'diamond', 'cube', 'cube', 'leaf', 'leaf', 'bicycle', 'bicycle'];
-const cardsDeck = document.querySelector('.deck');
-const restartGameBtn = document.querySelector('.game-restart');
+const cardsHolderArr = ["paper-plane-o", "paper-plane-o", "bolt", "bolt", "bomb", "bomb", "diamond", "diamond", "cube", "cube", "leaf", "leaf", "bicycle", "bicycle"];
+const cardsDeck = document.querySelector(".deck");
+const restartGameBtn = document.querySelector(".game-restart");
 
 //Game popup message of game variables
-const gameTime = document.querySelector('.game-time');
-const gameMessages = document.querySelector('.game-messages');
-const gameStars = document.querySelector('.stars');
-const gameMovesCounter = document.querySelector('.game-moves-counter');
-const gameMoves = document.querySelector('.game-moves');
+const gameTime = document.querySelector(".game-time");
+const gameMessages = document.querySelector(".game-messages");
+const gameStars = document.querySelector(".stars");
+const gameMovesCounter = document.querySelector(".game-moves-counter");
+const gameMoves = document.querySelector(".game-moves");
 
 let gameTimer;
-let gameMinutes = document.querySelector('.game-minutes');
-let gameSeconds = document.querySelector('.game-seconds');
+let gameMinutes = document.querySelector(".game-minutes");
+let gameSeconds = document.querySelector(".game-seconds");
 
 let resetGameTimer = 0;
 
 let cardsMatchingArr = [];
 let finishGameCardsCounter = 0;
 
+restartGameBtn.addEventListener("click", createGameBoard);
+cardsDeck.addEventListener("click", openAndShowCardSymbol);
+shuffle(cardsHolderArr);
+startGameBoard();
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+//create gameBoard and start the game
+function createGameBoard() {
+    cardsDeck.innerHTML = "";
+    shuffle(cardsHolderArr);
+    startGameBoard();
+    resetGameTimer = 0;
+    gameMinutes.innerText = 0;
+    gameSeconds.innerText = 0;
+    finishGameCardsCounter = 0;
+    gameMoves.innerText = 0;
+    stopWatch();
+    startRating();
+    hidePopupMessage();
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -44,6 +56,16 @@ function shuffle(array) {
     return array;
 }
 
+//creating start game board with all cards
+function startGameBoard() {
+    for (let i = 0; i < cardsHolderArr.length; i++) {
+        const newListItem = document.createElement("li");
+
+        newListItem.setAttribute("class", "card fa fa-" + cardsHolderArr[i]);
+        cardsDeck.appendChild(newListItem);
+    }
+}
+
 //create function to stop game watch, who counts the game time until winning the game
 function setGameTime() {
     gameTimer = setInterval(function () {
@@ -56,14 +78,43 @@ function setGameTime() {
     return gameTimer;
 }
 
+//creating watch and count the times, and stop when all cards are staying opened
+function createWatch() {
+    gameTimer = setInterval(function() {
+        gameSeconds.innerText++;
+        if(gameSeconds.innerText === 60) {
+            gameMinutes.innerText++;
+            gameSeconds.innerText = 0;
+
+        }
+    }, 1000);
+    return gameTimer;
+}
+
+//stop watch
+function stopWatch() {
+    clearInterval(gameTimer);
+}
+
 //create function to turn the game card
 function turnGameCard(e) {
-    e.target.classList.add('open');
-    e.target.classList.add('show');
+    e.target.classList.add("open");
+    e.target.classList.add("show");
+}
+
+//create function to hide popup messages
+function hidePopupMessage() {
+    const scoreBlock = document.querySelector(".score-panel");
+    gameMessages.style.display = "none";
+    restartGameBtn.appendChild(gameTime);
+    scoreBlock.prepend(gameStars);
+    scoreBlock.appendChild(gameTime);
+    scoreBlock.appendChild(gameMovesCounter);
+    scoreBlock.appendChild(restartGameBtn);
 }
 
 //create function to show popup messages
-function popupMessage() {
+function showPopupMessage() {
     const winnerMessage = document.querySelector(".game-message");
     gameMessages.style.display = "block";
     winnerMessage.appendChild(restartGameBtn);
@@ -74,30 +125,29 @@ function popupMessage() {
 
 //create function for matching of cards
 function matchedCards() {
-    cardsMatchingArr[0].classList.add('match');
-    cardsMatchingArr[1].classList.add('match');
+    cardsMatchingArr[0].classList.add("match");
+    cardsMatchingArr[1].classList.add("match");
     finishGameCardsCounter++;
 
     if (finishGameCardsCounter === 8) {
         clearInterval(gameTimer);
-        popupMessage();
+        showPopupMessage();
     }
 }
 
 //create function for no matching of cards
 function notMatchedCards() {
-    cardsMatchingArr[0].classList.add('no-match');
-    cardsMatchingArr[1].classList.add('no-match');
+    cardsMatchingArr[0].classList.add("no-match");
+    cardsMatchingArr[1].classList.add("no-match");
     setTimeout(function() {
-        cardsMatchingArr[0].classList.remove('open', 'show', 'no-match');
-        cardsMatchingArr[1].classList.remove('open', 'show', 'no-match');
+        cardsMatchingArr[0].classList.remove("open", "show", "no-match");
+        cardsMatchingArr[1].classList.remove("open", "show", "no-match");
         cardsMatchingArr = [];
-    }, 1000);
+    }, 800);
 }
 
 //create function to checks if cards are matched
 function areMatchedCards() {
-
     if (cardsMatchingArr[0].classList.value === cardsMatchingArr[1].classList.value) {
         setTimeout(function() {
             matchedCards();
@@ -114,13 +164,40 @@ function countMovesForRating() {
     gameMoves.innerText++;
 
     if (gameMoves.innerText > 17) {
-        document.querySelector('.stars li:first-child').classList.add('empty-star');
+        document.querySelector(".stars li:first-child").classList.add("empty-star");
 
     }
     if (gameMoves.innerText > 25) {
-        document.querySelector('.stars li:nth-child(2)').classList.add('empty-star');
+        document.querySelector(".stars li:nth-child(2)").classList.add("empty-star");
     }
 }
+
+//open card and show its symbol
+function openAndShowCardSymbol(e) {
+    if(resetGameTimer === 0) {
+        setGameTime();
+        resetGameTimer = 1;
+    }
+    if (cardsMatchingArr.length > 1) {
+        return;
+    }
+
+    if (e.target.tagName === "LI") {
+        turnGameCard(e);
+        cardsMatchingArr.push(e.target);
+        if(cardsMatchingArr.length === 2) {
+            areMatchedCards();
+            countMovesForRating();
+        }
+    }
+}
+
+//rating reset
+function startRating() {
+    document.querySelector(".stars li:first-child").classList.remove("empty-star");
+    document.querySelector(".stars li:nth-child(2)").classList.remove("empty-star");
+}
+
 
 
 /*
